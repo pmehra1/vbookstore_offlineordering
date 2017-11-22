6320544060755c3248dd491764a2aeaa5de1595b
Prateek Mehra <pmehra1@gmail.com>
2017-11-22 16:39:49 +0800

    1. Updated name of common module in pom.xml. 2. Loading xml files db as string constants

--- vbookstore-common/src/main/java/com/virtusa/sg/vbookstore/vbookstore_common/constants/Constants.java
+++ vbookstore-common/src/main/java/com/virtusa/sg/vbookstore/vbookstore_common/constants/Constants.java
@@ -1,12 +1,25 @@
 package com.virtusa.sg.vbookstore.vbookstore_common.constants;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStreamReader;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Constants {
 
+	private static Logger log = LoggerFactory.getLogger(Constants.class);
+
 	public static String BOOKSTORE_DATA_XML = "";
 	public static String CUSTOMER_DATA_XML = "";
+
+	static {
+		getDbFilesAsString();
+	}
 
 	public static String getBasePathForDataFiles() {
 		String basePath = "";
@@ -16,6 +29,35 @@ public class Constants {
 			e.printStackTrace();
 		}
 		return basePath;
+	}
+
+	private static void getDbFilesAsString() {
+		String bookstoreFile = getBasePathForDataFiles() + "vbookstoredb\\bookstore_data.xml";
+		String customerFile = getBasePathForDataFiles() + "vbookstoredb\\customer_data.xml";
+
+		BOOKSTORE_DATA_XML = getFileAsString(bookstoreFile);
+		CUSTOMER_DATA_XML = getFileAsString(customerFile);
+	}
+
+	private static String getFileAsString(String fileName) {
+		StringBuilder sb = new StringBuilder();
+
+		try {
+			FileInputStream fis = new FileInputStream(new File(fileName));
+
+			String fileAsString = "";
+			BufferedReader br = new BufferedReader(new InputStreamReader(fis));
+
+			while ((fileAsString = br.readLine()) != null) {
+				sb.append(fileAsString);
+			}
+		} catch (FileNotFoundException e) {
+			log.error("file " + fileName + " not found");
+		} catch (IOException e) {
+			log.error("file " + fileName + " io exception");
+		}
+
+		return sb.toString();
 	}
 
 }
